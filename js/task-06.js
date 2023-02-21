8d5716dc9bf004418c4c6fc03f403b5a2660c490
SolusBlade <kykysha59fafa@gmail.com>
2023-02-21 19:39:01 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,13 @@
 const textInput = document.querySelector("#validation-input");
 
 textInput.addEventListener("blur", () => {
-  if(textInput.value.length === textInput.dataset.length){
+  if(textInput.value.length == textInput.dataset.length){
+    textInput.classList.remove('invalid');
+    textInput.classList.add('valid');
+    } else {
       textInput.classList.remove('valid');
       textInput.classList.add('invalid');
-    } else {
-      textInput.classList.remove('invalid');
-      textInput.classList.add('valid');
+      
     }
   });
   // done
